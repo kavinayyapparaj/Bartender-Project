edeaa11f4417ea7f6606e67f9fa1bc9b0c894f2e
Kavin Ayyapparaj <33475202+kavinayyapparaj@users.noreply.github.com>
2022-05-02 23:58:48 -0600

    Create gantryhome.py

---
+++ gantryhome.py
@@ -0,0 +1,9 @@
+def gantrystayathome(self):
+    import serial
+    import time
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser.reset_input_buffer()
+    for i in range(2):
+        ser.write(b"4")
+        line = str(ser.readline().decode('utf-8').rstrip())
+
